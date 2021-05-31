ead2b275ac192df4d4208741f360b8b1a6c07bbc
Akeem C <javantieclarke@gmail.com>
2021-05-31 19:09:04 -0400

    added loops for each round

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,8 +11,10 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
+
+    while (enemyHealth > 0){
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    //window.alert("Welcome to Robot Gladiators!");
   
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -59,10 +61,15 @@ var fight = function(enemyName) {
     fight();
   }
 }
-  };
 
-  for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames [i]);
   }
+
+};
+
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
+}
 
   
